0fbcd18d2d1a857162bef0f284f9daa90238303a
Antonio Figueroa <antonio_figueroa@dell.com>
2024-08-01 12:37:35 -0500

    - Adds char, bool, decimal, and double interpolation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -7,8 +7,13 @@
         {
             string dogName = "Mara";
             int dogAge = 10;
-
-            Console.WriteLine("My dog's name is "+ dogName + ", She is " + dogAge + " years old");
+            char grade = 'A';
+            bool isFriendly = true;
+            decimal toyPrice = 5.99m;
+            double weight = 49.3245;
+            string friendlyVal = isFriendly? "is" : "is not";
+             
+            Console.WriteLine("My dog's name is "+ dogName + ", She is " + dogAge + " years old. She weights " + weight + " pounds. She " + friendlyVal+ " friendly. Her favorite toy is a rope and it costs $" + toyPrice + ". She got an " + grade + " on her behaviour training.");
         }
     }
 }
