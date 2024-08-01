d30095741bbb1ad2239a56eaf2625adfca392f0f
Antonio Figueroa <antonio_figueroa@dell.com>
2024-08-01 11:19:56 -0500

    - Adds dog name and age interpolation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -2,9 +2,13 @@
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Mara";
+            int dogAge = 10;
+
+            Console.WriteLine("My dog's name is "+ dogName + ", She is " + dogAge + " years old");
         }
     }
 }
